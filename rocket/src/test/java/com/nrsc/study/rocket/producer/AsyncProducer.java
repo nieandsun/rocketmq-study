532d6b13b495f7a1c40563b3cc3b8f2f364900d8
sunchuan <272420199@qq.com>
2020-05-17 21:19:29 +0800

    【消息中间件】--- RocketMQ核心概念介绍 + 生产者简介 + 消息存储简介2

--- rocket/src/test/java/com/nrsc/study/rocket/producer/AsyncProducer.java
+++ rocket/src/test/java/com/nrsc/study/rocket/producer/AsyncProducer.java
@@ -1,54 +1,57 @@
 package com.nrsc.study.rocket.producer;
 
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
-import org.apache.rocketmq.common.CountDownLatch2;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class AsyncProducer {
-    public static void main(String[] args) throws Exception {
-        // 实例化消息生产者Producer
-        DefaultMQProducer producer = new DefaultMQProducer("async-group");
-        // 设置NameServer的地址
+    public static void main(
+            String[] args) throws MQClientException, InterruptedException {
+        //生产者实例化
+        DefaultMQProducer producer = new DefaultMQProducer("async");
+        //指定rocket服务器地址
         producer.setNamesrvAddr("localhost:9876");
-        // 启动Producer实例
+        //启动实例
         producer.start();
+        //发送异步失败时的重试次数(这里不重试)
         producer.setRetryTimesWhenSendAsyncFailed(0);
 
-        int messageCount = 5;
-        // 根据消息数量实例化倒计时计算器 --- 注意，这里不是使用的juc包下的CountDownLatch
-
-        final CountDownLatch2 countDownLatch = new CountDownLatch2(messageCount);
-
+        int messageCount = 10;
+        final CountDownLatch countDownLatch = new CountDownLatch(messageCount);
         for (int i = 0; i < messageCount; i++) {
-            final int index = i;
-            // 创建消息，并指定Topic，Tag，keys和消息体
-            Message msg = new Message("Topic-async",
-                    "TagA-async",
-                    "OrderID188",
-                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            try {
+                final int index = i;
+                Message msg = new Message("TopicTest",
+                        "TagC",
+                        "OrderID" + index,
+                        ("Hello world " + index).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                //生产者异步发送
+                producer.send(msg, new SendCallback() {
+                    @Override
+                    public void onSuccess(SendResult sendResult) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d OK %s %n", index, new String(msg.getBody()));
+                    }
 
-            // SendCallback接收异步返回结果的回调
-            producer.send(msg, new SendCallback() {
-                @Override
-                public void onSuccess(SendResult sendResult) {
-                    System.out.printf("%-10d OK %s %n", index,
-                            sendResult.getMsgId());
-                }
-                @Override
-                public void onException(Throwable e) {
-                    System.out.printf("%-10d Exception %s %n", index, e);
-                    e.printStackTrace();
-                }
-            });
+                    @Override
+                    public void onException(Throwable e) {
+                        countDownLatch.countDown();
+                        System.out.printf("%-10d Exception %s %n", index, e);
+                        e.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-        // 等待5s
+        //Thread.sleep(1000);
         countDownLatch.await(5, TimeUnit.SECONDS);
-        // 如果不再发送消息，关闭Producer实例。
         producer.shutdown();
     }
 }
